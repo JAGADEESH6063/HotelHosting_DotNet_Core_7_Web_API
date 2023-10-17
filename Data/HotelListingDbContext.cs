8f634b37eb17c429fb8e37a82e66a5a19c65daa7
JAGADEE5H <neeruganti.jagadeesh@solera.com>
2023-10-17 17:07:38 +0530

    Entity Frame work setup.

---
+++ Data/HotelListingDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelHosting.Data
+    {
+    public class HotelListingDbContext : DbContext
+        {
+        public HotelListingDbContext(DbContextOptions options) : base(options){ }
+        }
+    }
